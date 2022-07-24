ccc24cf4f8a0b8ee90a81df7192dcf6c5b2412ce
IARAC-S <iaracampos.df@gmail.com>
2022-07-24 20:03:13 -0300

    atualizacao

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,55 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+
+    private String nome;
+    //local na memoria para guardar o conteudo
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    // obrigatoriamente insira conteudos unicos
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    void inscreverBootcamp(Bootcamp bootcamp) {}
+
+    void calcularTotalxp(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+}
